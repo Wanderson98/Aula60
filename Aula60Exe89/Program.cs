43f9b1de8284b9db3bfd4a09ddfc467f9a472be5
Wanderson Andrade <wmandrade81@gmail.com>
2022-04-20 13:42:56 -0300

    Novos exercicios

---
+++ Aula60Exe89/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula60Exe89
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetorA = { 98, 15, 42, 7, 86, 12, 4, 11, 2, 32, 32, 8, 71, 26, 44, };
+            double[] vetorB = { 98, 23, 3, 7, 34, 2, 34, 31, 2, 5, 3, 5, 6, 43, 44, }; ;
+            int contador = 0;
+
+            for (int i = 0; i < vetorA.Length; i++)
+            {
+                if (vetorA[i] == vetorB[i])
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("Número de vezes que o Vetor A e o Vetor B tem o mesmo número na mesma posição: " + contador);
+        }
+    }
+}
---
+++ Aula60Exe90/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula60Exe90
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetorA = new double[5];
+            int contador = 0;
+
+            for (int i = 0; i < vetorA.Length; i++)
+            {
+                Console.Write($"Digite o Numero #{i + 1}: ");
+                vetorA[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Digite um número: ");
+            double num = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < vetorA.Length; i++)
+            {
+                if (vetorA[i] == num)
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine($"O número digitado {num} aparece {contador} no vetor A");
+        }
+    }
+}
