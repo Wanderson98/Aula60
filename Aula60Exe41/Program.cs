58a1a77913b7ed57375759c52e1934f0c66b7f50
Wanderson Andrade <wmandrade81@gmail.com>
2022-04-20 13:24:16 -0300

    Adicionando Exercicios

---
+++ Aula60Exe41/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Aula60Exe41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] notas = new double[3];
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.Write("Digite a Nota #{0}: ", i+1);
+                notas[i] = double.Parse(Console.ReadLine());
+            }
+            Console.Write("Digite a Média dos Exercicios: ");
+            double mediaExe = double.Parse(Console.ReadLine());
+
+            Aluno aluno = new Aluno(notas, mediaExe);
+
+            aluno.MostrarConceito();
+        }
+    }
+
+    public class Aluno
+    {
+
+        public double[] Notas = new double[3];
+        public double MediaExercicios { get; set; }
+
+        public Aluno(double[] notas, double mediaExercicios)
+        {
+            Notas = notas;
+            MediaExercicios = mediaExercicios;
+        }
+
+        private double CalcularNota()
+        {
+            return (Notas[0] + (Notas[1] * 2) + (Notas[2] * 3) + MediaExercicios) / 7;
+        }
+
+        public void MostrarConceito()
+        {
+            double media = CalcularNota();
+            if (media < 6.0)
+            {
+                Console.WriteLine("Conceito D - Média: {0:F2}", media);
+            }
+            else if (media < 7.5)
+            {
+                Console.WriteLine("Conceito C - Média: {0:F2}", media);
+            }
+            else if (media < 9.0)
+            {
+                Console.WriteLine("Conceito B - Média: {0:F2}", media);
+            }
+            else
+            {
+                Console.WriteLine("Conceito A - Média: {0:F2}", media);
+            }
+        }
+    }
+
+}
---
+++ Aula60Exe84/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula60Exe84
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanho dos vetores: ");
+            int tamVetor = int.Parse(Console.ReadLine());
+            double[] vetorA = new double[tamVetor];
+            double[] vetorB = new double[tamVetor];
+            double[] vetorSoma = new double[tamVetor];
+
+            for (int i = 0; i < tamVetor; i++)
+            {
+                Console.Write($"Digite o Valor do Vetor A na Posição #{i}: ");
+                vetorA[i] = double.Parse(Console.ReadLine());
+                Console.Write($"Digite o Valor do Vetor B na Posição #{i}: ");
+                vetorB[i] = double.Parse(Console.ReadLine());
+
+                vetorSoma[i] = vetorA[i] + vetorB[i];
+            }
+
+            for (int i = 0; i < tamVetor; i++)
+            {
+                Console.WriteLine($"Resultado Da Soma do VetorA na casa[{i}] e do Vetor B na casa[{i}] é : {vetorSoma[i]}");
+            }
+        }
+    }
+}
